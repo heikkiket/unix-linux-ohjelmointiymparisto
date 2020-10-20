8bbbadfea4a553fbea4ececd4d0afd5cc1e1ee4e
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-10-20 21:47:54 +0300

    Pieni laskin

---
+++ teht1/calc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+double add(double a, int b);
+double subtract(double a, int b);
+double multiply(double a, int b);
+double divide(double a, int b);
+
+int main(int argc, char *argv[])
+{
+  double result = atoi(argv[1]);
+  char opt;
+  while((opt = getopt(argc, argv, "a:s:d:m:")) != -1) {
+    switch(opt) {
+    case 'a': result = add(result, atoi(optarg));
+      break;
+    case 's': result = subtract(result, atoi(optarg));
+      break;
+    case 'd': result = divide(result, atoi(optarg));
+      break;
+    case 'm': result = multiply(result, atoi(optarg));
+      break;
+    }
+  }
+
+  printf("%.1f\n", result);
+  return EXIT_SUCCESS;
+}
+
+double add(double a, int b) {
+  return a + b;
+}
+
+double subtract(double a, int b) {
+  return a - b;
+}
+
+double multiply(double a, int b) {
+  return a * b;
+}
+
+double divide(double a, int b) {
+  return a / b;
+}
