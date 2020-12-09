1f1e490476ab09910726d0e8480e80dc944d06b7
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-12-09 23:04:53 +0200

    Harj 6 teht 3-4

--- harj6/teht3-4.c
+++ harj6/teht3-4.c
@@ -14,25 +14,6 @@ void debug(char *message)
   fprintf(stderr, "%s\n", message);
 }
 
-void create_cmdline(char *cmdline, char **arguments)
-{
-  char *argument;
-  int size = strlen(cmdline);
-  char buf[size];
-  strcpy(buf, cmdline);
-  char *command;
-  command = strtok(buf, " ");
-
-  arguments[0] = command;
-  int id = 1;
-  while ((argument = strtok(NULL, " ")) != NULL) {
-    arguments[id] = argument;
-    id++;
-  }
-  arguments[id] = NULL;
-}
-
-
 void closepipe(int pipe[])
 {
   close(pipe[0]);
@@ -54,38 +35,12 @@ void setsighandler() {
   sigaction(SIGCHLD, &act, NULL);
 }
 
-int startchild(char *cmd, char *cmdline, int in_fd, int out_fd) {
-
-  int pid = 0;
-
-  if ((pid = fork()) == -1) {
-    perror("Fork error");
-    exit(EXIT_FAILURE);
-  } else if (pid == 0) {
-    char msg[256];
-    sprintf(msg, "Child starts with cmdline '%s'", cmdline);
-    debug(msg);
-
-    char *arguments[20];
-    create_cmdline(cmdline, arguments);
-
-    dup2(in_fd, STDIN_FILENO);
-    dup2(out_fd, STDOUT_FILENO);
-
-    execvp(cmd, arguments);
-
-  }
-
-  return pid;
-}
-
 void runmain(int childamount)
 {
   debug("Parent starts");
   setsighandler();
   while (stoppedchilds < childamount){
   }
-  exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
@@ -94,24 +49,52 @@ int main(int argc, char *argv[])
   int pipe2[2];
   pipe(pipe1);
 
-  int children[3];
-  children[0] = startchild("ps", "ps -A", pipe1[0], pipe1[1]);
-  close(pipe1[1]);
-  pipe(pipe2);
-  close(pipe2[0]);
-  children[1] = startchild("grep", "grep bash", pipe1[0], pipe2[1]);
-  close(pipe1[0]);
-  close(pipe2[1]);
+  int pid;
+  if ((pid = fork()) == -1) {
+    perror("Fork error");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
 
-  if (fork() == 0) {
-    char buf[1];
-    while (read(pipe2[0], &buf, 1) > 0) {
-      write(STDOUT_FILENO, buf, 1);
-    }
+    close(pipe1[0]);
+
+    dup2(pipe1[1], STDOUT_FILENO);
+
+    execlp("ps", "ps", "-A", NULL);
   }
+
+  pipe(pipe2);
+
+  if ((pid = fork()) == -1) {
+    perror("Fork error");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+
+    close(pipe1[1]);
+    close(pipe2[0]);
+
+    dup2(pipe1[0], STDIN_FILENO);
+    dup2(pipe2[1], STDOUT_FILENO);
+
+    execlp("grep", "grep", "bash", NULL);
+  }
+  closepipe(pipe1);
+
+  if((pid = fork()) == -1) {
+    perror("Fork error");
+    exit(EXIT_FAILURE);
+  }
+  else if (pid == 0) {
+
+    close(pipe2[1]);
+
+    dup2(pipe2[0], STDIN_FILENO);
+
+    execlp("wc", "wc", "-l", NULL);
+  }
+  closepipe(pipe2);
 
   runmain(2);
 
-  printf("Stopped childs at the end: %d\n", stoppedchilds);
+  fprintf(stderr, "Stopped childs at the end: %d\n", stoppedchilds);
   return 0;
 }
